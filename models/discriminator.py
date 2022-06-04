b0a6404bfa926bf162c5d3edb8e1b801e7a50000
William Luo <32350533+pkulwj1994@users.noreply.github.com>
2022-06-04 16:23:23 +0800

    Update discriminator.py

--- models/discriminator.py
+++ models/discriminator.py
@@ -1,23 +1,23 @@
-import tensorflow as tf
-from a_nice_mc.utils.layers import dense, lrelu
+# import tensorflow as tf
+# from a_nice_mc.utils.layers import dense, lrelu
 
 
-class Discriminator(object):
-    def __init__(self):
-        self.name = 'discriminator'
+# class Discriminator(object):
+#     def __init__(self):
+#         self.name = 'discriminator'
 
-    def __call__(self, x, reuse=True):
-        raise NotImplementedError(str(type(self)))
+#     def __call__(self, x, reuse=True):
+#         raise NotImplementedError(str(type(self)))
 
 
-class MLPDiscriminator(Discriminator):
-    def __init__(self, dims):
-        super(MLPDiscriminator, self).__init__()
-        self.dims = dims
+# class MLPDiscriminator(Discriminator):
+#     def __init__(self, dims):
+#         super(MLPDiscriminator, self).__init__()
+#         self.dims = dims
 
-    def __call__(self, x, reuse=True):
-        with tf.variable_scope(self.name, reuse=reuse):
-            for dim in self.dims:
-                x = dense(x, dim, activation_fn=lrelu)
-            y = dense(x, 1, activation_fn=tf.identity)
-        return y
+#     def __call__(self, x, reuse=True):
+#         with tf.variable_scope(self.name, reuse=reuse):
+#             for dim in self.dims:
+#                 x = dense(x, dim, activation_fn=lrelu)
+#             y = dense(x, 1, activation_fn=tf.identity)
+#         return y
