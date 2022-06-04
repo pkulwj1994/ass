e9e1fc3b0ad76859c875196153f61c541d04382c
William Luo <32350533+pkulwj1994@users.noreply.github.com>
2022-06-04 16:32:23 +0800

    Update __init__.py

--- objectives/bayes_logistic_regression/__init__.py
+++ objectives/bayes_logistic_regression/__init__.py
@@ -1,11 +1,8 @@
 import numpy as np
 import tensorflow as tf
-from a_nice_mc.objectives import Energy
-from a_nice_mc.utils.evaluation import batch_effective_sample_size as effective_sample_size
-from a_nice_mc.utils.evaluation import acceptance_rate
-from a_nice_mc.utils.logger import save_ess, create_logger
+from objectives import Energy
 
-logger = create_logger(__name__)
+import torch
 
 
 class BayesianLogisticRegression(Energy):
@@ -24,12 +21,18 @@ class BayesianLogisticRegression(Energy):
         self.x_dim = data.shape[1]
         self.y_dim = labels.shape[1]
         self.dim = self.x_dim * self.y_dim + self.y_dim
-        self.mu_prior = tf.ones([self.dim]) * loc
-        self.sig_prior = tf.ones([self.dim]) * scale
+        # self.mu_prior = tf.ones([self.dim]) * loc
+        # self.sig_prior = tf.ones([self.dim]) * scale
+        self.mu_prior = torch.ones([self.dim]) * loc
+        self.sig_prior = torch.ones([self.dim]) * scale
 
-        self.data = tf.constant(data, tf.float32)
-        self.labels = tf.constant(labels, tf.float32)
-        self.z = tf.placeholder(tf.float32, [batch_size, self.dim])
+        # self.data = tf.constant(data, tf.float32)
+        # self.labels = tf.constant(labels, tf.float32)
+        # self.z = tf.placeholder(tf.float32, [batch_size, self.dim])
+
+        self.data = torch.from_numpy(data)
+        self.labels = torch.from_numpy(labels)
+        # self.z = tf.placeholder(tf.float32, [batch_size, self.dim])
 
         if batch_size:
             self.data = tf.tile(
@@ -53,16 +56,16 @@ class BayesianLogisticRegression(Energy):
     def _vector_to_model(self, v):
         w = v[:, :-self.y_dim]
         b = v[:, -self.y_dim:]
-        w = tf.reshape(w, [-1, self.x_dim, self.y_dim])
-        b = tf.reshape(b, [-1, 1, self.y_dim])
+        w = torch.reshape(w, [-1, self.x_dim, self.y_dim])
+        b = torch.reshape(b, [-1, 1, self.y_dim])
         return w, b
 
     def energy_fn(self, v, x, y):
         w, b = self._vector_to_model(v)
         logits = tf.matmul(x, w) + b
-        ll = tf.nn.sigmoid_cross_entropy_with_logits(labels=y, logits=logits)
-        ll = tf.reduce_sum(ll, axis=[1, 2])
-        pr = tf.square((v - self.mu_prior) / self.sig_prior)
+        ll = torch.nn.sigmoid_cross_entropy_with_logits(labels=y, logits=logits)
+        ll = torch.reduce_sum(ll, axis=[1, 2])
+        pr = torch.square((v - self.mu_prior) / self.sig_prior)
         pr = 0.5 * tf.reduce_sum(pr, axis=1)
         return pr + ll
 
@@ -76,14 +79,8 @@ class BayesianLogisticRegression(Energy):
         v = np.std(z_, axis=0, dtype=np.float64)
         print('mean: {}'.format(m))
         print('std: {}'.format(v))
-        logger.info('Acceptance rate %.4f' % (acceptance_rate(z)))
-        ess = effective_sample_size(
-            z,
-            self.mean(), self.std() * self.std(),
-            logger=logger
-        )
-        if path:
-            save_ess(ess, path)
+
+
 
     @staticmethod
     def mean():
