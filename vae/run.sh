03242f5d2e29f9ca5f8ecc02dc411caf5c098709
zzzhangboya <zhangboya@pku.edu.cn>
2022-06-23 19:41:36 +0800

    Update run.sh

--- vae/run.sh
+++ vae/run.sh
@@ -1,3 +1,3 @@
 
-# python main.py --runner VAERunner --config vae/mnist_ass.yml --doc ass_test
+# python main.py --runner VAERunner --config vae/mnist_ass.yml --doc ass_test1
 python main.py --runner VAERunner --config vae/mnist_ass.yml --doc ass_test1 --test
